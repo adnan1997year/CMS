d17f62faaae1852e2cb611ea58769e62e0f4247a
DESKTOP-H2EOD6G\Adnan <adnanjatoi21@gmail.com>
2020-11-22 09:43:51 +0500

    Working on login page

--- cms/src/main/java/com/cms/controller/HomeController.java
+++ cms/src/main/java/com/cms/controller/HomeController.java
@@ -1,15 +1,10 @@
-package com.cms;
+package com.cms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-	@GetMapping("/")
-	public String home() {
-		return "login_registration";
-	}
 	
 	@GetMapping("/complain")
 	public String complain() {
